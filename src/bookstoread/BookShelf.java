36e0a67bb81b01ebc8dcf8586e0a454e2ed159d6
konekaridja <kalyakone656@gmail.com>
2026-05-20 16:22:22 +0000

    codeE methode arrange refactorisé

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -16,13 +16,11 @@ public class BookShelf {
     }
 
     public List<Book> arrange() {
-        return books.stream()
-                .sorted()
-                .collect(Collectors.toList());
+        return arrange(Comparator.naturalOrder());
     }
-
     public List<Book> arrange(Comparator<Book> criteria) {
         return books.stream().sorted(criteria).collect(Collectors.toList());
     }
+
 
 }
