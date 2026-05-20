3953e718e6c5e6be1b277bce5ce0a7979426ea45
kalyakone656-netizen <kalyakone656@gmail.com>
2026-05-20 21:56:44 +0000

    Ajout de groupByPublicationYear()

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,8 +1,9 @@
 package bookstoread;
 
+import java.time.Year;
 import java.util.*;
 import java.util.stream.Collectors;
-
+import java.time.Year;
 public class BookShelf {
 
     private final List<Book> books = new ArrayList<>();
@@ -21,6 +22,11 @@ public class BookShelf {
     public List<Book> arrange(Comparator<Book> criteria) {
         return books.stream().sorted(criteria).collect(Collectors.toList());
     }
-
+    public Map<Year, List<Book>> groupByPublicationYear() {
+        return books.stream().collect(
+                Collectors.groupingBy(book ->
+                        Year.of(book.getPublishedOn().getYear()))
+        );
+    }
 
 }
