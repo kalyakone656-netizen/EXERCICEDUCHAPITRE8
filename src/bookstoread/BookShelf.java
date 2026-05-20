8f02011a9cd5e8514da91d078110c4785fad1ce0
konekaridja <kalyakone656@gmail.com>
2026-05-20 13:35:27 +0000

    liste de livre rendue non modifiable

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,17 +1,21 @@
 package bookstoread;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class BookShelf {
     private final List<String> books = new ArrayList<>();
 
     public List<String> books() {
-        return books;
+        return Collections.unmodifiableList(books);
     }
-
     public void add(String... booksToAdd) {
         books.addAll(Arrays.asList(booksToAdd));
     }
