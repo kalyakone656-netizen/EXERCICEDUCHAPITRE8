17dcd3ae78989de739767a2367b9842067a860ee
konekaridja <kalyakone656@gmail.com>
2026-05-20 16:16:43 +0000

    Possibilité de trier selon n'importe quelle critère

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,26 +1,28 @@
 package bookstoread;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class BookShelf {
-    private final List<String> books = new ArrayList<>();
 
-    public List<String> books() {
+    private final List<Book> books = new ArrayList<>();
+
+    public List<Book> books() {
         return Collections.unmodifiableList(books);
     }
-    public void add(String... booksToAdd) {
+
+    public void add(Book... booksToAdd) {
         books.addAll(Arrays.asList(booksToAdd));
     }
 
-    public List<String> arrange() {
-        return books.stream().sorted().collect(Collectors.toList());
+    public List<Book> arrange() {
+        return books.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public List<Book> arrange(Comparator<Book> criteria) {
+        return books.stream().sorted(criteria).collect(Collectors.toList());
     }
 
 }
