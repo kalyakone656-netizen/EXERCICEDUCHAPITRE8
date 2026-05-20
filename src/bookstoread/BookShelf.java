57a22288aec54af528be49b2fbf44a9e98ac10d8
kalyakone656-netizen <kalyakone656@gmail.com>
2026-05-20 22:05:15 +0000

    Refactorisation de groupByPublicationYear() avec groupBy()

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -25,9 +25,8 @@ public class BookShelf {
         return books.stream().sorted(criteria).collect(Collectors.toList());
     }
     public Map<Year, List<Book>> groupByPublicationYear() {
-        return books.stream().collect(
-                Collectors.groupingBy(book ->
-                        Year.of(book.getPublishedOn().getYear()))
+        return this.groupBy(book ->
+                Year.of(book.getPublishedOn().getYear())
         );
     }
     public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx) {
